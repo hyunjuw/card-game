c95b4c374dd0010923fbd71872a720af97b2775b
woohyunju <whj0531@openobject.net>
2023-11-16 13:36:28 +0900

    card-game 초기셋팅

---
+++ src/components/card.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div>card</div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/cardView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import card from '@/components/card.vue'
+</script>
+
+<template>
+  <main>
+    <card />
+  </main>
+</template>
